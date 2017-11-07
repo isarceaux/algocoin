79d2f0833c0123c240304ccea01a49c7223962b4
isarceaux <isarceaux>
2017-11-07 19:07:53 +0100

    script to seed currencies done

---
+++ app/services/seed_currencies.rb
@@ -0,0 +1,22 @@
+class SeedCurrencies
+
+  URL_FIX_ZERO    = 'https://poloniex.com/public?command='
+
+  def initialize
+  end
+
+  def perform
+    currencies = HTTParty.get( url_build('returnCurrencies') ).parsed_response
+    currencies.each do |c|
+      cnew = Currency.new
+      cnew.code = c[0]
+      cnew.name = c[1]["name"]
+      cnew.save
+    end
+  end
+
+  def url_build( command )
+    return URL_FIX_ZERO + command
+  end
+
+end
