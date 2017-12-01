344260c137793491cca97608982a0d532c9f44e1
isarceaux <isarceaux>
2017-12-01 18:47:21 +0100

    changed background color of active menu

--- app/controllers/arbitrages_controller.rb
+++ app/controllers/arbitrages_controller.rb
@@ -2,7 +2,7 @@ class ArbitragesController < ApplicationController
 
   def index
     @order_book_count = OrderBook.all.count
-    @barrier = 0.9
+    @barrier = 1
     @arbitrage_selection = Arbitrage.where("raw_ratio > ?",@barrier)
   end
 
