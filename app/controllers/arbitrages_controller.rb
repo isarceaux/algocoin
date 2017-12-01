b6089c172f133b231eee83feef56e7654b8dd82a
isarceaux <isarceaux>
2017-12-01 17:53:33 +0100

    added a table for the arbitrage view

--- app/controllers/arbitrages_controller.rb
+++ app/controllers/arbitrages_controller.rb
@@ -3,7 +3,7 @@ class ArbitragesController < ApplicationController
   def index
     @arbitrage_count = Arbitrage.all.count
     @order_book_count = OrderBook.all.count
-    @barrier = 1
+    @barrier = 0.9
     @arbitrage_selection = Arbitrage.where("raw_ratio > ?",@barrier)
   end
 
