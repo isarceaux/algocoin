7d317fb6986352bd8a8a5203cbbdbfd5507df38f
isarceaux <isarceaux>
2017-12-12 17:29:30 +0100

    changed the selection of objects for the calculation of the worst ratio

--- app/services/worst_ratio_calculation.rb
+++ app/services/worst_ratio_calculation.rb
@@ -6,7 +6,7 @@ class WorstRatioCalculation
 
   def perform
     worst_ratio = 0
-    Arbitrage.all.each do |arbitrage|
+    Arbitrage.where('worst_ratio':nil).each do |arbitrage|
       if arbitrage.first_order_book && arbitrage.second_order_book && arbitrage.third_order_book
         worst_ratio = 1/arbitrage.first_order_book.ask_hash.first[0].to_f * 1/arbitrage.second_order_book.ask_hash.first[0].to_f * arbitrage.third_order_book.bid_hash.last[0].to_f 
         arbitrage.worst_ratio = worst_ratio
