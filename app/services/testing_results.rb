0b126215518e40cae75eda987b33223265c25084
isarceaux <isarceaux>
2018-06-04 18:08:42 +0200

    established a process to try to identify why some trades don't pass, it seems to be specific to some currencies, to be checked deployed

--- app/services/testing_results.rb
+++ app/services/testing_results.rb
@@ -1,30 +1,40 @@
 class TestingResults
 
-  def initialize
+  def initialize(first_id,last_id)
 
     missed_arbitrages = []
     successful_arbitrages = []
+    winning_shitcoins = []
+    losing_shitcoins = []
 
-    for i in 112..123
+    for i in first_id..last_id
       if Arbitrage.find(i).trades == []  
         missed_arbitrages << Arbitrage.find(i)
+        losing_shitcoins << Arbitrage.find(i).trio.third_currency.code
       else
         successful_arbitrages << Arbitrage.find(i)
+        winning_shitcoins << Arbitrage.find(i).trio.third_currency.code
       end
     end
 
+    
+
     puts "List of successful arbitrages"
     puts "-----------------------------"
-    successful_arbitrages.each do |arbitrage|
-      puts "#{arbitrage.id}: " "#{arbitrage.trio.first_currency.code}" + "->" + "#{arbitrage.trio.second_currency.code}" + "->" "#{arbitrage.trio.third_currency.code}"
-    end
+    # successful_arbitrages.each do |arbitrage|
+    #   puts "#{arbitrage.id}: " "#{arbitrage.trio.first_currency.code}" + "->" + "#{arbitrage.trio.second_currency.code}" + "->" "#{arbitrage.trio.third_currency.code}"
+    # end
+    puts winning_shitcoins.sort
 
     puts ""
     puts "List of failed arbitrages"
     puts "-----------------------------"
-    missed_arbitrages.each do |arbitrage|
-      puts "#{arbitrage.id}: " "#{arbitrage.trio.first_currency.code}" + "->" + "#{arbitrage.trio.second_currency.code}" + "->" "#{arbitrage.trio.third_currency.code}"
-    end
+    # missed_arbitrages.each do |arbitrage|
+    #   puts "#{arbitrage.id}: " "#{arbitrage.trio.first_currency.code}" + "->" + "#{arbitrage.trio.second_currency.code}" + "->" "#{arbitrage.trio.third_currency.code}"
+    # end
+    puts losing_shitcoins.sort
+
+    # binding.pry()
 
   end
   
