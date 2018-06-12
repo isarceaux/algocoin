0cc943dc2d999f7a1c58992ae69604f54a496f3d
isarceaux <isarceaux>
2018-06-12 16:54:53 +0200

    minor edits

--- app/services/sidekiq_launch.rb
+++ app/services/sidekiq_launch.rb
@@ -2,13 +2,13 @@ class SidekiqLaunch
 
   def initialize(number_of_times, ratio_value)
     
-    # number_of_times.times do
-    #   CalculatingArbitrageWorker.perform_async(ratio_value)
-    # end
-
     number_of_times.times do
       MakingArbitrageWorker.perform_async(ratio_value)
     end
+
+    # number_of_times.times do
+    #   CalculatingArbitrageWorker.perform_async(ratio_value)
+    # end
 
     ## To clear data uncomment these lines
     # OrderBook.pluck('id').each do |id|
